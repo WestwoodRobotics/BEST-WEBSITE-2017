436a6a31c6564135185c6e19092c18dc963ec1b6
Lokraan <Lokraan@users.noreply.github.com>
2017-10-04 21:40:14 -0500

    Fix Github link, upload jQuery changes

--- jquery.responsive.js
+++ jquery.responsive.js
@@ -18,33 +18,4 @@ window.onload = function() {
 		});
 	});
 
-	$(window).resize(function() {
-		var $width = $(this).width();
-		var $height = $(this).height();
-
-		var $conHeight = 
-	})
-
-	// $(window).scroll(function() {
-	// 	console.log("scroll");
-	// 	$(".container.content div").each(function() {
-	// 		var $visible = $(this).visible(true);
-	// 		// if($(this).is("hover")) {
-
-	// 		// }
-	// 		if($(this).is("hover") || $visible) {
-	// 			var divId = $(this).attr("id");
-	// 			console.log(divId);
-	// 			$(".navbar-nav li a").each(function() {
-	// 				if($(this).attr("href").indexOf(divId) > -1 && !$(this).hasClass("active")) {
-	// 					console.log($(this).attr("href"));
-	// 					$(".active").removeClass("active");
-	// 					$(this).parent().addClass("active");
-	// 					return false;
-	// 				}
-	// 			});
-	// 			return false;	
-	// 		}
-	// 	});
-	// });
 }
